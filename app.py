43b5076dfbf351458e37cac6b5d93dfd992e1508
anurag <a1chaudhari@ucsd.edu>
2024-07-16 00:19:32 +0100

    second page flsak testing

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/hello')
+def hello():
+    return render_template('hello.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
