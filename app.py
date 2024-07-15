10176d430dbe1c688e49d89971b461d2a753371c
anurag <a1chaudhari@ucsd.edu>
2024-07-16 00:29:43 +0100

    flask file setup get and post requests for index

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
     result = None
     if request.method == 'POST':
