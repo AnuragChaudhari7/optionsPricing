0e682bfbe8e9b97df5f242082f88428081e78acf
anurag <a1chaudhari@ucsd.edu>
2024-07-16 14:43:01 +0100

    implemented binomial fast model

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-from models.binomial_european import binomial_slow
+from models.binomial_european import binomial_fast
 
 app = Flask(__name__)
 
@@ -15,7 +15,7 @@ def index():
         N = int(request.form['N'])
         r = float(request.form['r'])
         u = float(request.form['u'])
-        result = round(float(binomial_slow(S0=S0, K=K, T=T, N=N, r=r, u=u)), 2) #currently default values in funciton
+        result = round(float(binomial_fast(S0=S0, K=K, T=T, N=N, r=r, u=u)), 2) #currently default values in funciton
     return render_template('index.html', result=result)
 
 @app.route('/hello')
--- models/binomial_european.py
+++ models/binomial_european.py
@@ -10,6 +10,25 @@ u = 1.2     # up factor
 d = 1/u     # down factor (inverse for recombining tree)
 opt = 'C'   # options type (C - call ; P - put)
 
+def binomial_fast(S0=100,K=103,T=1,N=3,r=0.06,u=1.2,d=1/u,opt='C'):
+    # first compute equation constants
+    dt = T/N 
+    p = (np.exp(r*dt) - d)/(u - d)
+    discount = np.exp(-r * dt)
+
+    #initialise array with stock prices
+    C = S0 * u ** (np.arange(0,N+1,1)) * d ** (np.arange(N,-1,-1))
+
+    #calculate option payoffs using vectors
+    C = np.maximum(C-K, np.zeros(N+1))
+
+    #iterate backwards and multiply vectors so that each index in previous step corresponds
+    #to the same index times the one above it - hence the slicing
+    for i in np.arange(N,0,-1):
+        C = discount * (p * C[1:i+1] + (1-p) * C[0:i])
+    
+    return C[0]
+
 def binomial_slow(S0=100,K=103,T=1,N=3,r=0.06,u=1.2,d=1/u,opt='C'):
     # first compute equation constants
     dt = T/N 
@@ -35,3 +54,4 @@ def binomial_slow(S0=100,K=103,T=1,N=3,r=0.06,u=1.2,d=1/u,opt='C'):
             C[j] = discount * (p * C[j+1] + (1-p) * C[j]) 
     
     return C[0].item()
+
