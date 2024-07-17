b6b6d9a5d5e97f04e89779a79ec25ff7da335a27
anurag <a1chaudhari@ucsd.edu>
2024-07-17 15:57:29 +0100

    added binomial put function to backend code

--- models/binomial_european.py
+++ models/binomial_european.py
@@ -22,3 +22,7 @@ def binomial_call(S0=100,K=103,T=1,N=3,r=0.06,u=1.2,d=1/1.2):
     
     return C[0].item()
 
+def binomial_put_from_call(C,S0=100,K=103,T=1,r=0.06):
+    discount = np.exp(-r * T)
+    P = C + (K * discount) - S0
+    return P
