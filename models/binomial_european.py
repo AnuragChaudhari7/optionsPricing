4a9ad94f749da287244c37363bcfce13fbe5218f
anurag <a1chaudhari@ucsd.edu>
2024-07-22 17:13:11 +0100

    changed deafult parameters

--- models/binomial_european.py
+++ models/binomial_european.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 
-def binomial_call(S0=100,K=103,T=1,N=3,r=0.06,sig=0.2):
+def binomial_call(S0=42,K=40,T=1,N=3,r=0.06,sig=0.2):
     # first compute equation constants
     dt = T/N 
     u = math.exp(sig * math.sqrt(dt))
@@ -22,7 +22,7 @@ def binomial_call(S0=100,K=103,T=1,N=3,r=0.06,sig=0.2):
     
     return C[0].item()
 
-def binomial_put_from_call(C,S0=100,K=103,T=1,r=0.06):
+def binomial_put_from_call(C,S0=42,K=40,T=1,r=0.06):
     discount = np.exp(-r * T)
     P = C + (K * discount) - S0
     return P
