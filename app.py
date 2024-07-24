aba8b20e7a289d653ba3e7e6b8019dfacb19decb
AnuragC-473 <146322274+AnuragChaudhari7@users.noreply.github.com>
2024-07-24 23:39:25 +0100

    binomial call react and flask linked

--- app.py
+++ app.py
@@ -1,30 +1,48 @@
 from flask import Flask, render_template, request, jsonify
 from models.binomial_european import binomial_call
 from models.binomial_european import binomial_put_from_call
-from time import time
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:3000"])
 
-@app.route('/', methods=['GET','POST'])
-def index():
-    call_price = None
-    put_price = None
-    if request.method == 'POST':
+@app.route('/api/european/binomial', methods=['POST'])
+def european_binomial():
+    data = request.get_json()
+    S0 = float(data.get('S0'))
+    K = float(data.get('K'))
+    T = float(data.get('T'))
+    N = int(data.get('N'))
+    r = float(data.get('r'))
+    sig = float(data.get('sig'))
+    
+    call_price = binomial_call(S0=S0, K=K, T=T, N=N, r=r,sig=sig)
+    result = {"result": round(call_price, 2)}
+    return jsonify(result)
 
-        S0 = float(request.form['S0'])
-        K = float(request.form['K'])
-        T = float(request.form['T'])
-        N = int(request.form['N'])
-        r = float(request.form['r'])
-        sig = float(request.form['sig'])
 
-        call_price = binomial_call(S0=S0, K=K, T=T, N=N, r=r,sig=sig)
-        put_price = binomial_put_from_call(C=call_price,S0=S0,K=K,T=T,r=r)
+# @app.route('/', methods=['GET','POST'])
+# def index():
+#     call_price = None
+#     put_price = None
+#     if request.method == 'POST':
 
-        call_price = round(call_price,2)
-        put_price = round(put_price,2)
+#         S0 = float(request.form['S0'])
+#         K = float(request.form['K'])
+#         T = float(request.form['T'])
+#         N = int(request.form['N'])
+#         r = float(request.form['r'])
+#         sig = float(request.form['sig'])
 
-    return render_template('index.html', call_price=call_price, put_price = put_price)
+#         call_price = binomial_call(S0=S0, K=K, T=T, N=N, r=r,sig=sig)
+#         put_price = binomial_put_from_call(C=call_price,S0=S0,K=K,T=T,r=r)
+
+#         call_price = round(call_price,2)
+#         put_price = round(put_price,2)
+
+#     return render_template('index.html', call_price=call_price, put_price = put_price)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
