e9a49ccc182663f55d14784e58e1e21d0ef4a543
AnuragC-473 <146322274+AnuragChaudhari7@users.noreply.github.com>
2024-07-24 12:07:57 +0100

    install axios

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 def your_python_function(input_data):
     # Replace with your function logic
