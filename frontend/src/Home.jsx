d3479300e39cc0039e538b2664782ec55cd88be6
anurag <a1chaudhari@ucsd.edu>
2024-07-25 14:04:44 +0100

    home page

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h2>Welcome to the Options Pricing App</h2>
+            <p>This is the home page.</p>
+        </div>
+    );
+}
+
+export default Home;
