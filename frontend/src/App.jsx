ecf6798c957fbc2d6e5b5f024d633e2ce6645f2d
anurag <a1chaudhari@ucsd.edu>
2024-07-30 13:32:22 +0100

    black-scholes frontend implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import Binomial from './Binomial';
+import BlackScholes from './BlackScholes';
 
 
 function App() {
@@ -12,8 +13,9 @@ function App() {
         <header className="App-header">
           <h1 className='App-title'>Options Pricing Models</h1>
             <nav className="App-nav">
-                   <Link to="/">Home</Link>
-                    <Link to="/binomial">Binomial</Link>
+              <Link to="/">Home</Link>
+              <Link to="/binomial">Binomial</Link>
+              <Link to="/blackscholes">Black-Scholes</Link>
             </nav>
             
         </header>
@@ -21,6 +23,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/binomial" element={<Binomial />} />
+                <Route path="/blackscholes" element={<BlackScholes />} />
             </Routes>
         </main>
     </div>
