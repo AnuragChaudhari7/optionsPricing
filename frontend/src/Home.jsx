74a907a0e2588361067f47b6855e62398906c353
anurag <a1chaudhari@ucsd.edu>
2024-07-31 19:12:31 +0100

    removed homepage links, react routing to be done

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -9,10 +9,10 @@ function Home() {
                 to determine the fair price of an option. Currently there are 2 models present.
             </p>
             <ol>
-                <li><a href='https://optionspricing-frontend.onrender.com/binomial'>Binomial Tree Model</a></li>
+                <li>Binomial Tree Model</li>
                 <p>The Binomial Tree Model generates a binomial tree of 'N' steps to predict stock movement and 
                     hence derive the fair price of the option</p>
-                <li><a href='https://optionspricing-frontend.onrender.com/blackscholes'>Black-Scholes Model</a></li>
+                <li>Black-Scholes Model</li>
                 <p>The Black Scholes model is in the context of continuous time models. Its similar to the Binomial Tree 
                     model except the time steps between each stock movement tends to infinity, creating a continuous movement.
                     This model involves solving a Partial Differential Eqation.
