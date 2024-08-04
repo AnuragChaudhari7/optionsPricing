236208b8f2b7a61c8ca89a193079978343623880
anurag <a1chaudhari@ucsd.edu>
2024-08-04 15:00:28 +0100

    added timer function

--- frontend/src/Valuations.jsx
+++ frontend/src/Valuations.jsx
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Valuations.css';
 
 function Valuations(){
     const [valuationTable, setValuationTable] = useState();
-    const [ticker, setTicker] = useState('aapl')
+    const [ticker, setTicker] = useState('aapl');
+    const [timer, setTimer] = useState(30);
 
     const renderTable = (table) => {
         if (!table) return null;
 
-        let rows = []
-        let columns = []
+        let rows = [];
+        let columns = [];
         const keys = Object.keys(table);
         for(let i = 0; i < Object.keys(table[keys[0]]).length; i++){
             let row = {};
             keys.forEach(key => {
-                row[key] = table[key][i]
+                row[key] = table[key][i];
             });
             rows.push(row);
         }
@@ -43,14 +44,29 @@ function Valuations(){
 
     const getTable = async () => {
         try {
-            const response = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/api/realtime/valuations`, {ticker:ticker});
-            setValuationTable(response.data)
+            const response = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/api/realtime/valuations`, { ticker });
+            setValuationTable(response.data);
         } catch (error) {
             console.error('Error fetching result:', error);
         }
     };
 
+    useEffect(() => {
+        getTable();
+        setTimer(30);
 
+        const intervalId = setInterval(() => {
+            setTimer((prevTimer) => {
+                if (prevTimer === 1){
+                    getTable();
+                    return 30;
+                }
+                return prevTimer - 1;
+            });
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, [ticker]);
 
     return (
         <div className='Valuations'>
@@ -73,19 +89,17 @@ function Valuations(){
                 </select>
                 </div>
 
-                <div className='Valuations-button' >
+                {/*<div className='Valuations-button' >
                     <button  onClick={getTable}>Display Valuations</button>
-                </div>
+                </div>*/}
             </div>
-            
+            <br></br>
+            <h2>Time till next update: {timer} seconds</h2>
             <h3>Data Table:</h3>
             {/*valuationTable ? renderTable(valuationTable) : <h3>Select Ticker & Press Display</h3>*/}
             {renderTable(valuationTable) || <h4>Select Ticker & Press Display</h4>}
-
         </div>
-    ) 
-    
-    
+    );
 }
 
 export default Valuations;
